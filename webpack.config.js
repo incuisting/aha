051e83238ebfcba34a2a9347da9fbb6e11e6aa95
xuyanting <393146040@qq.com>
2018-11-09 09:29:07 +0800

    提取loader配置到一个单独的文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
+const { loaderRules } = require('./webpackConfig/loaderConfig')
 module.exports = {
   entry: {
     main: path.resolve(__dirname, 'src/index.js')
@@ -17,81 +17,7 @@ module.exports = {
     open: true
   },
   module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env', 'stage-0', 'react'],
-            plugins: ['transform-decorators-legacy']
-          }
-        },
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          }
-        ],
-        exclude: /node_modules/,
-        include: path.resolve(__dirname, 'src')
-      },
-      {
-        test: /\.less/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          },
-          'less-loader'
-        ]
-      },
-      {
-        test: /\.scss/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          },
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(jpg|png|bmp|gif|svg|ttf|woff|woff2|eot)/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              //小于这个大小的图片会变成base64 格式
-              limit: 4096
-            }
-          }
-        ]
-      }
-    ]
+    rules: loaderRules
   },
   plugins: [
     new HtmlWebpackPlugin({
---
+++ webpackConfig/loaderConfig.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const cssLoader = {
+  loader: 'css-loader',
+  options: {
+    modules: true
+  }
+}
+
+module.exports = {
+  loaderRules: [
+    {
+      test: /\.jsx?$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'stage-0', 'react'],
+          plugins: ['transform-decorators-legacy']
+        }
+      },
+      exclude: /node_modules/
+    },
+    {
+      test: /\.css$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true
+          }
+        }
+      ],
+      exclude: /node_modules/,
+      include: path.resolve(__dirname, '../src')
+    },
+    {
+      test: /\.less/,
+      include: path.resolve(__dirname, '../src'),
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true
+          }
+        },
+        'less-loader'
+      ]
+    },
+    {
+      test: /\.scss/,
+      include: path.resolve(__dirname, '../src'),
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true
+          }
+        },
+        'sass-loader'
+      ]
+    },
+    {
+      test: /\.(jpg|png|bmp|gif|svg|ttf|woff|woff2|eot)/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            //小于这个大小的图片会变成base64 格式
+            limit: 4096
+          }
+        }
+      ]
+    }
+  ]
+}
