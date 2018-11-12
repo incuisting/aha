51d87b78ead6e3db04f2e506239a0bcfb6243eb8
xuyanting <393146040@qq.com>
2018-11-12 14:31:29 +0800

    add redux-logger

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './reducers'
 import App from './components/App'
-const store = createStore(rootReducer)
+import store from './store'
 render(
   <Provider store={store}>
     <App />
