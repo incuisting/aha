cd8c6da2b0d5d14d15cfe87c9bb43ccbb5859477
xuyanting <393146040@qq.com>
2018-11-14 14:56:03 +0800

    add webpack bundle optimize

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,18 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
-
+const UglifyJSplugin = require('uglifyjs-webpack-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  optimization: {
+    //代码压缩
+    minimizer: [
+      new UglifyJSplugin({
+        cache: true, //启用缓存
+        parallel: true, // 使用多进程运行改进编译速度
+        sourceMap: true //生成sourceMap映射文件
+      }),
+      new OptimizeCssAssetsWebpackPlugin({})
+    ]
+  }
 })
