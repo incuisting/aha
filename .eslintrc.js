e4d689b1a9f06fc18863dad7af13199e6d4b585e
xuyanting <393146040@qq.com>
2018-12-25 13:42:43 +0800

    添加eslint airbnb 代码规则

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: ["airbnb"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  globals: {
+    document: false
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import Router from './router'
-import Nav from './components/Nav'
-// import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
-// import App from './components/App'
-// import Counter from './containers/Counter'
-import store from './store'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import Router from './router';
+import store from './store';
 
 render(
   <Provider store={store}>
     <Router />
   </Provider>,
-  document.getElementById('app')
-)
+  document.getElementById('app'),
+);
