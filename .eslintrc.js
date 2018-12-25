f9750e6baab8a21079cc5faa475b613616b7bd20
xuyanting <393146040@qq.com>
2018-12-25 14:54:49 +0800

    eslint config and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-  parser: "babel-eslint",
-  extends: ["airbnb"],
-  rules: {
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-  },
-  env: {
-    browser: true,
-    node: true
-  },
-  globals: {
-    document: false
-  }
+    parser: 'babel-eslint',
+    extends: ['airbnb'],
+    rules: {
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    },
+    env: {
+        browser: true,
+        node: true,
+    },
+    //全局变量提示
+    globals: {
+        document: false,
+    },
 };
