50d9eac1a3425b4d8a54fcf8181c84d3a9119d1c
xuyanting <393146040@qq.com>
2018-12-26 13:16:48 +0800

    precommit test 8

--- src/store.js
+++ src/store.js
@@ -3,7 +3,6 @@ import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 import rootReducer from './reducers';
 import rootSaga from './sagas';
-
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware, logger];
 const store = createStore(rootReducer, applyMiddleware(...middleware));
