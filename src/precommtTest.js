ee9bb274e477b056649601e7b0153b609340ba2b
xuyanting <393146040@qq.com>
2018-12-26 13:21:37 +0800

    precommit test 10

--- src/precommtTest.js
+++ src/precommtTest.js
@@ -1,1 +1,11 @@
-console.log('a' === 1);
+import React from 'react';
+
+export default class A extends React.Component {
+  bb = () => {
+    console.log('11');
+  };
+
+  render() {
+    return <div>11</div>;
+  }
+}
